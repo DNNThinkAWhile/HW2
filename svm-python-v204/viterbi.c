8ba8747fc2b6691d89327e754b64018837433f87
weitang114 <weitang114@DarrendeMacBook-Pro.local>
2015-05-03 17:45:27 +0800

    Fix some viterbi.c bugs, finish inference

--- svm-python-v204/viterbi.c
+++ svm-python-v204/viterbi.c
@@ -1,9 +1,10 @@
-#include<string.h>
-#include<stdio.h>
-#include<assert.h>
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
 
 // Cost function
-int SimpleDiffCost (char* const A, char* const B) 
+int SimpleDiffCost (char* const A, char* const B)
 {
     printf("Input strings: %s", A);
     printf(" and %s\n", B);
@@ -16,22 +17,40 @@ int SimpleDiffCost (char* const A, char* const B)
     return Diff;
 }
 
-#define LENGTH  69
-#define WIDTH   48
+
+void printArray(double* d, int len) {
+    printf("print array:");
+    for (int i = 0; i < len; i++) {
+        printf("%f ", d[i]);
+    }
+    puts("");
+}
+
+void printArrayI(int* d, int len) {
+    printf("print array:");
+    for (int i = 0; i < len; i++) {
+        printf("%d ", d[i]);
+    }
+    puts("");
+}
+
+
+#define LENGTH  3//69
+#define WIDTH   2//48
 // W is 1-base
-char* inference (double w[],  double x[] ,  int x_length)
+int* inference (double w[],  double x[] ,  int x_length)
 {
-    double *bigmap = (double*)malloc(x_length*WIDTH);
-    int *backmap = (int*)malloc(x_length*WIDTH);
-    char *path = (char*)malloc(x_length);
+    double *bigmap = (double*)calloc(x_length*WIDTH, sizeof(double));
+    int *backmap = (int*)calloc(x_length*WIDTH, sizeof(int));
+    int *path = (int*)calloc(x_length, sizeof(int));
     // Observation array
     double ob[LENGTH*WIDTH];
-    for (int i = 0; i < LENGTH*WIDTH; i ++) 
+    for (int i = 0; i < LENGTH*WIDTH; i ++)
         ob[i] = w[i + 1]; // 1-base
     // Transition array
     double tr[WIDTH*WIDTH];
     for (int i = 0; i < WIDTH*WIDTH ; i ++ )
-        tr[i] = w[LENGTH*WIDTH + i]; // 1-base
+        tr[i] = w[LENGTH*WIDTH + i + 1]; // 1-base
     // Matrix Multiply
     for (int i = 0; i < WIDTH; i ++) {
         for (int j = 0; j < x_length; j ++) {
@@ -40,7 +59,7 @@ char* inference (double w[],  double x[] ,  int x_length)
                 tmp_sum += x[j*LENGTH + i69] * ob[WIDTH*i69 + i];
             }
             bigmap[WIDTH*j + i] = tmp_sum;
-       } 
+       }
     }
 
     // Viterbi
@@ -48,13 +67,14 @@ char* inference (double w[],  double x[] ,  int x_length)
     double MaxEndValue = 0.0;
     for (int j = 1 ; j < x_length; j ++) {
         for (int i = 0 ; i < WIDTH; i ++) {
-            double tmpMax = 0.0;
+            double tmpMax = -1 << 20;
             for (int index = 0 ; index < WIDTH ; index ++) {
                 double compare = bigmap[WIDTH*(j-1) + index] + tr[index*WIDTH + i];
                 if (tmpMax < compare) {
                     tmpMax = compare;
-                    backmap[x_length*j + i] = index;
-                    }
+                    backmap[WIDTH*j + i] = index;
+                    printf("set backmap %d, %d\n", WIDTH*j+i, index);
+                }
             }
             bigmap[WIDTH*j + i] += tmpMax;
             if (j == x_length - 1) {
@@ -67,20 +87,57 @@ char* inference (double w[],  double x[] ,  int x_length)
     }
 
     // Backtrace
-    for (int i = x_length - 1 ; i >= 0 ; i --) {
-        // TODO
+    int maxIdx = MaxEndIndex;
+    path[x_length - 1] = maxIdx;
+    for (int i = x_length - 1 ; i > 0 ; i --) {
+        printf("%d, %d\n", WIDTH*i + maxIdx, maxIdx);
+        maxIdx = backmap[WIDTH * i + maxIdx];
+
+        path[i - 1] = maxIdx;
     }
+
+    // for (int i = 0; i < 3; i++) {
+    //     printf("%d, %f\n", path[i], bigmap[WIDTH * i + path[i]]);
+    // }
+
     free(bigmap);
     free(backmap);
+
     return path;
 }
 
 char* find_most_violated_constraint ()
 {
-
+    return NULL;
 }
 
 int main () {
+
+
+    double w[] = {
+        0.0,
+        // obs
+        0.1, 0.5,
+        0.3, 0.1,
+        0.2, 0.2,
+
+        // trans
+        0.2, 0.8,
+        0.6, 0.4
+    };
+
+    double x[] = {
+        2, 1, 3,
+        1, 3, 1,
+        2, 3, 3
+    };
+
+    // should be 1->0->1
+    int* d = inference(w, x, 3);
+
+    for (int i = 0; i < 3; i++) {
+        printf("%d\n", d[i]);
+    }
 
     return 0;
 }
