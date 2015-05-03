ffb0a09777fa9ccf184c79c18201dba051666c89
GB Lin <b00901056@ntu.edu.tw>
2015-05-03 20:20:50 +0800

    find_most_violated done

--- svm-python-v204/viterbi.c
+++ svm-python-v204/viterbi.c
@@ -115,61 +115,77 @@ int* inference (double w[],  double x[] ,  int x_length)
     return path;
 }
 
-char* find_most_violated (double w[], double x[], char* y, int x_length)
+int* find_most_violated (double w[], double x[], int y[], int x_length)
 {
-   double *bigmap = (double*)malloc(x_length*WIDTH);
-   int *backmap = (int*)malloc(x_length*WIDTH);
-   char* path = (char*)malloc(x_length);
-   // Ob
-   double ob[LENGTH*WIDTH];
-   for (int i = 0;i < LENGTH*WIDTH; i ++)
-       ob[i] = w[i + 1];
-   // Tr
-   double tr[WIDTH*WIDTH];
-   for (int i = 0; i < WIDTH*WIDTH; i ++)
-       tr[i] = w[LENGTH+WIDTH + i];
-   // Matrix Mult.
-   for (int i = 0 ; i < WIDTH ; i ++) {
-       for (int j = 0 ; j < x_length; j ++) {
-           double tmp_sum = 0.0;
-           for (int i69 = 0 ; i69 < LENGTH ; i69 ++)
-               tmp_sum += x[j*x_length + i69] * ob[WIDTH*i69 + i];
-           bigmap[WIDTH*j + i] = tmp_sum;
+    double *bigmap = (double*)calloc(x_length*WIDTH, sizeof(double));
+    int *backmap = (int*)calloc(x_length*WIDTH, sizeof(int));
+    int *path = (int*)calloc(x_length, sizeof(int));
+    int *lossmap = (int*)calloc(x_length*WIDTH, sizeof(int));
+    // Observation array
+    double ob[LENGTH*WIDTH];
+    for (int i = 0; i < LENGTH*WIDTH; i ++)
+        ob[i] = w[i + 1]; // 1-base
+    // Transition array
+    double tr[WIDTH*WIDTH];
+    for (int i = 0; i < WIDTH*WIDTH ; i ++ )
+        tr[i] = w[LENGTH*WIDTH + i + 1]; // 1-base
+    // Matrix Multiply
+    for (int i = 0; i < WIDTH; i ++) {
+        for (int j = 0; j < x_length; j ++) {
+            double tmp_sum = 0.0;
+            for (int i69 = 0; i69 < LENGTH ; i69 ++) {
+                tmp_sum += x[j*LENGTH + i69] * ob[WIDTH*i69 + i];
+            }
+            bigmap[WIDTH*j + i] = tmp_sum;
        }
-   }
+    }
 
-   // Viterbi
-   int MaxEndIndex = 0;
-   double MaxEndValue = 0.0;
-   for (int j = 1 ; j < x_length ; j ++) {
-       for (int i = 0 ; i < WIDTH ; i ++) {
-           double tmpMax =0.0;
-           for (int index = 0 ; index < WIDTH ; index ++) {
-               double compare = bigmap[WIDTH*(j-1) + index] + tr[index*WIDTH + i]; // + SimpleDiffCost()
-               if (tmpMax < compare) {
-                   tmpMax = compare;
-                   backmap[x_length*j + i] = index;
-               }
-           }
-           bigmap[WIDTH*j + i] += tmpMax;
-           if (j == x_length - 1) {
-               if (bigmap[WIDTH*j + i] > MaxEndValue) {
-                   MaxEndValue = bigmap[WIDTH*j + i];
-                   MaxEndIndex = i;
-               }
-           }
-       }
-   }
+    // Viterbi
+    int MaxEndIndex = 0;
+    double MaxEndValue = 0.0;
+    for (int i = 0 ; i < WIDTH; i ++) 
+        lossmap[i] = (i == y[0] ? 0 : 1);
+    
+    for (int j = 1 ; j < x_length; j ++) {
+        for (int i = 0 ; i < WIDTH; i ++) {
+            double tmpMax = -1 << 20;
+            for (int index = 0 ; index < WIDTH ; index ++) {
+                double compare = bigmap[WIDTH*(j-1) + index] + tr[index*WIDTH + i] + lossmap[WIDTH*(j-1) + index];
+                if (tmpMax < compare) {
+                    tmpMax = compare;
+                    backmap[WIDTH*j + i] = index;
+                    lossmap[WIDTH*j + i] = lossmap[WIDTH*(j-1) + index] + (i == y[j] ? 0 : 1);
+                    printf("set backmap %d, %d\n", WIDTH*j+i, index);
+                }
+            }
+            bigmap[WIDTH*j + i] += tmpMax;
+            if (j == x_length - 1) {
+                if (bigmap[WIDTH*j + i] > MaxEndValue) {
+                    MaxEndValue = bigmap[WIDTH*j + i];
+                    MaxEndIndex = i;
+                }
+            }
+        }
+    }
 
-   // Backtrace
-   for (int i = x_length - 1 ; i >= 0 ; i ++) {
-       // TODO
-   }
+    // Backtrace
+    int maxIdx = MaxEndIndex;
+    path[x_length - 1] = maxIdx;
+    for (int i = x_length - 1 ; i > 0 ; i --) {
+        printf("%d, %d\n", WIDTH*i + maxIdx, maxIdx);
+        maxIdx = backmap[WIDTH * i + maxIdx];
 
+        path[i - 1] = maxIdx;
+    }
 
-   free(bigmap);
-   free(backmap);
-   return path;
+    // for (int i = 0; i < 3; i++) {
+    //     printf("%d, %f\n", path[i], bigmap[WIDTH * i + path[i]]);
+    // }
+
+    free(bigmap);
+    free(backmap);
+
+    return path;
 }
 
 int main () {
